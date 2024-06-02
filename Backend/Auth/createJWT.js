caab0b484d1f70f074d7d58c27d2aa2044fb75fe
IslamTamjid <towhidulislam24@protonmail.com>
2024-06-03 01:52:05 +0600

    a minor change made

--- Backend/Auth/createJWT.js
+++ Backend/Auth/createJWT.js
@@ -2,14 +2,8 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 const createToken = (user, expiresIn) => {
-    const { name, gender, email, password, phone, address, dob } = user;
-    return jwt.sign({ name,
-            gender,
-            email,
-            password,
-            phone,
-            address,
-            dob, }, process.env.ACCESS_TOKEN, { expiresIn: expiresIn });
+    const { email } = user;
+    return jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: expiresIn });
 }
 
 export default createToken;
