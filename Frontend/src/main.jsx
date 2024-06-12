4fe5836a0ab2c57cf512be535a9e33b87e52bb03
IslamTamjid <towhidulislam24@protonmail.com>
2024-06-13 01:22:46 +0600

    inital structure

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  Route, 
+  RouterProvider, 
+  createBrowserRouter, 
+  createRoutesFromElements
+} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
