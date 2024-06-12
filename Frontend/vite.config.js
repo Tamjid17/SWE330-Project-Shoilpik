ab3a6fb157e09c4aad2b95abeb4c4db49354d3d6
IslamTamjid <towhidulislam24@protonmail.com>
2024-06-13 01:57:38 +0600

    components installation

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:4000',
+      "/api": "http://localhost:4000",
     },
   },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
