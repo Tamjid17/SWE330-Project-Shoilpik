3afb0802db9be34f2b07c77165ee4bdaa6495c63
IslamTamjid <towhidulislam24@protonmail.com>
2024-11-10 10:10:27 +0600

    resolve conflict 3

--- Backend/Routes/products.js
+++ Backend/Routes/products.js
@@ -1,29 +1,32 @@
 import express from "express";
 import {
-    getAllproductsController,
-    getProductsByIdController,
-    getProductsBySellerIdController,
-    createProductImageController,
-    updateProductController,
-    deleteProductControler,
-    getEveryProducts
+  getAllproductsController,
+  getProductsByIdController,
+  getProductsBySellerIdController,
+  createProductImageController,
+  updateProductController,
+  deleteProductControler,
+  getEveryProducts,
 } from "../controllers/products.js";
 import { upload } from "../Auth/multer.js";
 import { verifyToken } from "../Auth/authCheck.js";
 
 const productRouter = express.Router();
-productRouter.get("/", getAllproductsController);
-productRouter.get("/:id", verifyToken, getProductsByIdController);
-productRouter.get("/seller/:seller_id", verifyToken, getProductsBySellerIdController);
-productRouter.put("/:id", verifyToken, updateProductController);
+
+productRouter.get("/all", getAllproductsController);
+productRouter.get("/:id", getProductsByIdController);
+productRouter.get(
+  "/seller/:seller_id",
+  verifyToken,
+  getProductsBySellerIdController
+);
 productRouter.post(
   "/listing",
   verifyToken,
   upload.single("file"),
   createProductImageController
 );
+productRouter.put("/:id", verifyToken, updateProductController);
 productRouter.delete("/:id", verifyToken, deleteProductControler);
 
 export default productRouter;
-//Sazzad
-
